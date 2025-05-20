77ec911dbe0c09289e31b7a4e7e14b50785c68f0
MohamedWael257 <mohamedwael4553@gmail.com>
2025-05-20 15:56:41 +0300

    first commit

--- app/[lng]/auth/change-password/page.tsx
+++ app/[lng]/auth/change-password/page.tsx
@@ -1,8 +1,5 @@
 import ChangePassword from "@/components/auth/change-password/ChangePassword";
-import BreadcrumbSlugs from "@/shared/Breadcrumbs/CustomBreadcrumbs";
-
 export default function ChangePasswordPage() {
-  const paths = [{ name: "Set a new password" }];
 
   return <ChangePassword />;
 }
